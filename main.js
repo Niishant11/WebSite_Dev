99aff21f1481646738138746054aad806b5ace3d
Nishant Pandey <146617899+Niishant11@users.noreply.github.com>
2024-05-11 11:01:23 +0530

    File Added.
    I have added pervious file of portfolio website.

---
+++ main.js
@@ -0,0 +1,18 @@
+//Setup and start animation
+
+var typed = new typed('.pfdis', {
+    strings: ["Web Developer", "Coder", "Software Developer", "Tech. Lover"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+});
+
+
+<!-- Setup and start animation! -->
+<script>
+  var typed = new Typed('#element', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 50,
+  });
+</script>
